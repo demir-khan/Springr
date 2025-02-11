c8c535d6f0cff5a9379a2e5587bac7509868f3a5
Demir <tigerprince2006@icloud.com>
2025-02-11 16:12:12 +0000

    Initial Commit

---
+++ Springr/SpringrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringrApp.swift
+//  Springr
+//
+//  Created by Demir Khan on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpringrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpringrTests/SpringrTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpringrTests.swift
+//  SpringrTests
+//
+//  Created by Demir Khan on 11/02/2025.
+//
+
+import Testing
+
+struct SpringrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
